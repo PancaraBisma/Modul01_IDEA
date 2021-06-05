0c424471ee1d7352a154918abb05c40d628a3696
PancaraBisma <pancarabisma@gmail.com>
2021-06-05 19:45:53 +0800

    Project IDEA

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        String namaDepan = "Muhammad";
+        String namaTengah = "Pancara";
+        String namaBelakang = "Bisma";
+        int usia = 19;
+        int targetTahunKuliah = 4;
+        double ipk = 3.84832;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
